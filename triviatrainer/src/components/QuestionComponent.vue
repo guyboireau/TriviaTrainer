fc39dff0e23250815b3266b943ccf4c424d281e8
Quidi boz <boireauguy@gmail.com>
2024-02-23 12:00:37 +0100

    module question dfonctionnel

--- triviatrainer/src/components/QuestionComponent.vue
+++ triviatrainer/src/components/QuestionComponent.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <h2>{{ question.category }}</h2>
-    <h2>{{ question.question }}</h2>
-    <h2>{{ question.type }} - {{ question.difficulty }}</h2>
-    <!-- {{ question.incorrect_answers.push(q uestion.correct_answer) }} -->
-    <!-- <p>{{ question.correct_answer }}</p> -->
-    <button v-for='answer in shuffledAnswers' :key='answer'>{{ answer }}</button>
+  <div class="question-card">
+    <div v-if="question" class="card">
+      <h2>{{ (question.category).charAt(0).toUpperCase() + (question.category).slice(1) }}</h2>
+      <h3>{{ question.question.text }}</h3>
+      <p>{{ question.difficulty }}</p>
+      <div class="answers">
+        <button v-for="answer in shuffledAnswers" :key="answer" @click="checkAnswer(answer)">{{ answer }}</button>
+      </div>
+    </div>
+    <div v-else>
+      <p>Aucune question à afficher</p>
+    </div>
   </div>
 </template>
 
-<script lang='ts'>
-import { ref, defineProps } from 'vue'
-import { Options, Vue } from 'vue-class-component'
+<script setup lang="ts">
+import { ref, onMounted, defineProps } from 'vue'
 
 const shuffle = (array: string[]) => {
   let currentIndex = array.length
@@ -29,36 +33,53 @@ const shuffle = (array: string[]) => {
 }
 
 const props = defineProps({
-  question: String,
-  type: String,
-  difficulty: String,
-  correctAnswer: String,
-  incorrectAnswers: Array
+  question: Object
 })
 
-const correctAnswer = ref(props.correctAnswer)
-const incorrectAnswers = ref(props.incorrectAnswers)
+const shuffledAnswers = ref<string[]>([])
+const correctAnswers = ref(0)
+const incorrectAnswers = ref(0)
 
-const answers = [correctAnswer.value, ...(incorrectAnswers.value as string[])]
-const shuffledAnswers = shuffle(answers.filter(answer => answer !== undefined) as string[])
-
-@Options({
-  props: {
-    question: String,
-    category: String,
-    type: String,
-    difficulty: String,
-    correctAnswer: String,
-    incorrectAnswers: Array
+onMounted(() => {
+  if (props.question) {
+    const answers = [...props.question.incorrectAnswers, props.question.correctAnswer]
+    shuffledAnswers.value = shuffle(answers)
   }
 })
 
-export default class QuestionComponent extends Vue {
-  question!: string
-  category!: string
-  type!: string
-  difficulty !: string
-  correctAnswer !: string
-  incorrectAnswers !: [string]
+const checkAnswer = (selectedAnswer: string) => {
+  if (props.question && selectedAnswer === props.question.correctAnswer) {
+    correctAnswers.value++
+  } else {
+    incorrectAnswers.value++
+  }
 }
 </script>
+
+<style scoped>
+.question-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card {
+  background-color: #f0f0f0;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 80%;
+  max-width: 600px;
+}
+
+.answers {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+button {
+  margin: 5px;
+}
+</style>
--- triviatrainer/src/views/HomeView.vue
+++ triviatrainer/src/views/HomeView.vue
@@ -1,14 +1,7 @@
-<template>
-  <div class='home'>
-    <!-- <img alt='Vue logo' src='../assets/logo.png'> -->
-    <QuestionComponent v-for='question in state.questions' :key='question' :question='question' />
-  </div>
-</template>
-
 <script lang='ts'>
 import { Options, Vue } from 'vue-class-component'
 import QuestionComponent from '@/components/QuestionComponent.vue'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 // const initToken = async () => {
 //   const token = localStorage.getItem('token') || ''
@@ -20,39 +13,47 @@ import { ref } from 'vue'
 //     return newToken
 //   }
 // }
-// const token = initToken()
 // const fetchToken = async () => {
 //   const response = await fetch('https://opentdb.com/api_token.php?command=request')
 //   return (await response.json()).token
 // }
+
+// const token = initToken()
+
 const fetchQuestion = async () => {
-  fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+  fetch('https://the-trivia-api.com/v2/questions')
     .then((res) => res.json())
     .then((data) => {
-      state.value.questions = data.results
+      state.questions.push(...data)
+      // console.log(state.questions)
     })
 }
 
 fetchQuestion()
 
-const state = ref({
+const state = reactive({
   questions: [
     {
-      category: ref(''),
-      type: ref(''),
-      difficulty: ref(''),
-      question: ref(''),
-      correct_answer: ref(''),
-      incorrect_answers: ref([])
+      id: String,
+      question: {
+        text: String
+      },
+      category: String,
+      type: String,
+      difficulty: String,
+      correctAnswer: String,
+      incorrectAnswers: [String]
     }
   ]
 })
+state.questions.splice(0, 1)
 
 @Options({
   components: {
     QuestionComponent
   }
 })
+
 export default class HomeView extends Vue {
   state = state
   data (): object {
@@ -62,3 +63,12 @@ export default class HomeView extends Vue {
   }
 }
 </script>
+
+<template>
+  <div class='home'>
+    <!-- <img alt='Vue logo' src='../assets/logo.png'> -->
+    <div v-if='state.questions!==null'>
+      <QuestionComponent v-for='question in state.questions' :key='question.id' :question='question' />
+    </div>
+  </div>
+</template>
